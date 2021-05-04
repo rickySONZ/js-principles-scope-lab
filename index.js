e10e5bc9fcc466075de64b4416a07223bbf2edf1
Ryan Erricson <ryanerricson@gmail.com>
2021-05-04 12:35:55 -0400

    commit tree e21df8c514d48c1cbf362373a9b880c531f6e17b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+      customerName = customerName.toUpperCase();
+      return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(input){
+    bestCustomer = input;
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'Adrian';
+
+function changeLeastFavoriteCustomer(input) {
+    leastFavoriteCustomer = input
+    return leastFavoriteCustomer
+}
+
